d2efb4f3c8d2a67bc77c7b0223c6100cafac848b
reboot-student <Alexandermurfitt@hotmail.com>
2025-06-12 13:52:16 +0000

    ppppp

--- book_recommender_api/app/database.py
+++ book_recommender_api/app/database.py
@@ -6,9 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-
 client = MongoClient(MONGO_URI)
 db = client["book_recommender"]
 
+# Devuelve el objeto base de la base de datos
 def get_db():
     return db
+
+# Devuelve directamente la colección de libros
+def get_books_collection():
+    return db["books"]
--- book_recommender_api/app/models.py
+++ book_recommender_api/app/models.py
@@ -1,5 +1,5 @@
 # models.py
-# ✅ models.py
+
 from pydantic import BaseModel
 from typing import List
 
@@ -22,3 +22,21 @@ class Personality(BaseModel):
 class FullProfile(BaseModel):
     preferences: Preferences
     personality: Personality
+
+class BookOut(BaseModel):
+    title: str
+    author: str
+    genres: List[str]
+    subgenres: List[str]
+    themes: List[str]
+    emotion_tags: List[str]
+    tone: str
+    style: str
+    age_range: str
+    personality_match: List[str]
+    year: int
+    description: str
+
+class RecommendationResponse(BaseModel):
+    recommendation: BookOut
+    explanation: str
